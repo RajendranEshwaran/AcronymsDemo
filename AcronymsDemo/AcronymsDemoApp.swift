43ba02312fb7e5faa92193add0b921749ceb1c01
RajendranEshwaran <rajendranmca2010@gmail.com>
2022-02-10 13:47:26 -0500

    Initial Commit

---
+++ AcronymsDemo/AcronymsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcronymsDemoApp.swift
+//  AcronymsDemo
+//
+//  Created by gomathi saminathan on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AcronymsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
